f2ab37880544b0dc88ef52a73d146b596590cc75
Julien Garret <julien.garretb@gmail.com>
2015-06-30 00:48:47 +0200

    modification des page login de foseuserbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Elycee\ElyceeBundle\ElyceeElyceeBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new dashboard\dashboardBundle\dashboarddashboardBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
