95afa3b40d923e07dcd5346b311819e34323745b
juju921 <julien.garretb@gmail.com>
2015-07-30 08:20:35 +0200

    maj

--- src/dashboard/dashboardBundle/Controller/studentController.php
+++ src/dashboard/dashboardBundle/Controller/studentController.php
@@ -80,7 +80,7 @@ class studentController extends Controller
 
 
 
-        $form = $this->createFormBuilder($score) ;
+        $form = $this->createFormBuilder($copie) ;
              foreach ($fiche->getChoices() as $choice) {
                  if($choice->getResponse() == 0){
                      $tata  = 0;
@@ -88,27 +88,36 @@ class studentController extends Controller
                      $tata  = 1;
                  }
 
-                 $content = $choice->getContentChoice();
-                 $form->add($content,'choice',array(
+                $content = $choice->getContentChoice();
+                $form->add($content,'choice',array(
                      'label' =>$content,
-
                      'expanded'  => true,
                      'mapped'    => false,
-                     'required'  => false
+                     'required'  => false,
+
+                 ));
+              /*   $form->add('reponse' , 'radio', array(
+                     'required' => false,
+
                  ));
 
-
+                 $form->add('save', 'submit', array('label' => 'envoyer mes réponses',
+                     'attr' => array('class' => 'btn btn-primary'),
+                 ));*/
 
               }
 
 
-            $form = $form->getForm()->handleRequest($request);
+
+
+        $form = $form->getForm()->handleRequest($request);
         if ($request->isMethod('POST')) {
-            echo '<pre>';Debug::dump($form->getData() );echo '</pre>';exit();
+           // echo '<pre>';Debug::dump($form->getData() );echo '</pre>';exit();
+           // echo '<pre>';Debug::dump($form->getData('reponse') );echo '</pre>';
 
             if ($form->isValid() && $form->isSubmitted()) {
                 $data = $form->all();
-                $em->persist($copie);
+                $em->persist($score);
                 $em->flush();
                 
             }
